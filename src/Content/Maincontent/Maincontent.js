73ed94a4e69e18882e3facbe790bd573a927b056
A <M>
2020-06-15 19:52:39 +0300

    containers modified one more time

--- src/Content/Maincontent/Maincontent.js
+++ src/Content/Maincontent/Maincontent.js
@@ -5,7 +5,7 @@ import PostContainer from "./Post/PostContainer";
 
 const Maincontent = (props) => {
 
-    let routeOfLinks = props.state.links.linkData.map(
+    let routeOfLinks = props.linkData.map(
         (r) => {
             return <Route path={r.path}>{r.subLinks.map(
                 (n) => { return <NavLink to={n.childPath}>{n.childName}</NavLink> })}</Route>
@@ -38,10 +38,10 @@ const Maincontent = (props) => {
                 <button onClick={addpost}>Add post</button>
                 <div className={style.postModal}>
                     <div className={style.queationBox}>
-                        <textarea onChange={newPostText} value={props.state.posts.newPostData.newPostQuestion} ref={newQuestionArea} className={style.questionArea} placeholder="Enter your question"></textarea>
+                        <textarea onChange={newPostText} value={props.newPostQuestion} ref={newQuestionArea} className={style.questionArea} placeholder="Enter your question"></textarea>
                     </div>
                     <div className={style.answerBox} >
-                        <textarea onChange={newPostText} ref={newAnswerArea} className={style.answerArea} placeholder="Enter your answer" value={props.state.posts.newPostData.newPostAnswer}></textarea>
+                        <textarea onChange={newPostText} ref={newAnswerArea} className={style.answerArea} placeholder="Enter your answer" value={props.newPostAnswer}></textarea>
                     </div>
                 </div>
 
--- src/Content/Maincontent/MaincontentContainer.js
+++ src/Content/Maincontent/MaincontentContainer.js
@@ -21,7 +21,7 @@ const MaincontentContainer = (props) => {
     }
 
     return (
-        <Maincontent state={props.state} addCommentCont={addCommentCont} addNewCommentTextCont={addNewCommentTextCont} addpostCont={addpostCont} newPostTextCont={newPostTextCont} dispatch={props.dispatch} postData={props.state.posts.postData} commentData={props.state.posts.postData.commentData} newCommentData={props.state.posts.newCommentData}/>
+        <Maincontent addCommentCont={addCommentCont} addNewCommentTextCont={addNewCommentTextCont} addpostCont={addpostCont} newPostTextCont={newPostTextCont} dispatch={props.dispatch} postData={props.state.posts.postData} commentData={props.state.posts.postData.commentData} newCommentData={props.state.posts.newCommentData} linkData={props.state.links.linkData} newPostAnswer={props.state.posts.newPostData.newPostAnswer} newPostQuestion={props.state.posts.newPostData.newPostQuestion}/>
     );
 
 }
